2e4b971c92c9f339cb9387ce249e8dc42d98fe22
JacobF94 <jacob.frensley@gmail.com>
2022-03-04 11:59:22 -0600

    nearly done

---
+++ insulter/csharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+Main();
+void Main()
+{
+    List<string> Insults = new List<string>()
+    {
+        "Insult one",
+        "insult two",
+        "insult three",
+        "insult four",
+        "insult five"
+    };
+
+    List<int> RandomIndexes = new List<int>();
+    Random r = new Random();
+    List<int> RandomNumbers = new List<int>();
+    while (RandomNumbers.Count < 3)
+    {
+        int RandomIndex = r.Next(1, 5);
+        if (!RandomNumbers.Contains(RandomIndex))
+        {
+            RandomNumbers.Add(RandomIndex);
+        }
+    }
+    foreach (int index in RandomNumbers)
+    {
+        Console.WriteLine($"{Insults[index]}");
+    }
+
+
+
+};
---
+++ wish-list/csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+List<string> WishList = new List<string>()
+{
+    "Wish number 1",
+    "Wish number 2",
+    "Wish number 3"
+};
+
+foreach (string wish in WishList)
+{
+    Console.WriteLine($"{wish}");
+}
